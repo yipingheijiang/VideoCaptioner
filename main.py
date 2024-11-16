d72caa890b28935f3bfcc18e198aa9b722e08734
“weifeng2333” <2715673327@qq.com>
2024-11-16 21:09:17 +0800

    fix/root-path-error #14

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import os
 import sys
 
 # 添加项目根目录到 Python 路径
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.dirname(current_dir)
+project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(project_root)
 
 # 修复中文路径问题
